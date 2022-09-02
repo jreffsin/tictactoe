889189a00e7cba1ae514b9a43bf91a98abf5133c
Jesse Reffsin <jreffsin@gmail.com>
2022-09-02 15:11:59 -0400

    rough in board with sizing and marker logic

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 //next step: make gameboard responsive while maintaining aspect ratio
 
+//global variables
+let active_player = 'x';
+
 const populateBoard = function () {
     const game_board = document.getElementById('game_board');
     for (let i = 0; i < 9; i++){
@@ -10,7 +13,32 @@ const populateBoard = function () {
     }
 };
 
+// adds X to gameboard
+const addX = function (e) {
+    let marker = document.createElement('img');
+    if (active_player === 'x'){
+        marker.setAttribute('src', 'Assets/x_icon.svg');
+        active_player = 'o';
+    } else {
+        marker.setAttribute('src', 'Assets/o_icon.svg');
+        active_player = 'x';
+    }
+    marker.setAttribute('width', '100px');
+    e.target.appendChild(marker);
+    console.log('something');
+};
 
+// add click event to squares to turn them into X
+const addClickListeners = function () {
+    let game_squares = document.querySelectorAll('.game_square');
+
+    for (let i = 0; i < game_squares.length; i++){
+        game_squares[i].addEventListener('click', addX);
+    };
+};
+
+// function that determines last placed piece and changes next game piece
+// computer logic
 
 
 
@@ -18,4 +46,4 @@ const populateBoard = function () {
 
 
 populateBoard();
-
+addClickListeners();
