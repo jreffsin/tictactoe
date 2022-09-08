75a791f271f15a1df08f72a54f5856a1febbe6d6
Jesse Reffsin <jreffsin@gmail.com>
2022-09-07 22:08:27 -0400

    implemented computer move logic for turns 1 and 2

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-//global variables
+//init global variables
 let active_player = 'x';
 let gameType = 'pvp'
 let compTurn = 0;
@@ -79,24 +79,17 @@ let gameBoard = function () {
     let rowIndex = '';
     let boardArray = [];
 
-    boardArray = new Array(3);
+    const initBoardArray = function () {
+        boardArray = new Array(3);
         for (let i = 0; i < 3; i++) {
             boardArray[i] = new Array(3);
             for (let j = 0; j < 3; j++){
                 boardArray[i][j] = '-'
             }
-        }
+        };
+    };
 
-    // const initBoardArray = function () {
-    //     boardArray = new Array(3);
-    //     for (let i = 0; i < 3; i++) {
-    //         boardArray[i] = new Array(3);
-    //         for (let j = 0; j < 3; j++){
-    //             boardArray[i][j] = '-'
-    //         }
-    //     };
-    //     console.log(boardArray);
-    // };
+    initBoardArray();
 
     const drawDiagonalLine = function (leftStart) {
             if (leftStart === 'bot'){
@@ -179,10 +172,294 @@ let gameBoard = function () {
         }
     };
 
-    const takeCompTurn = function () {
-        if (gameType === 'pvc') {
-            computerPlayer.takeTurn();
+    const checkCompWin = function () {
+        let winChecker = '';
+        let won = false;
+
+        //check rows for winning move
+        if (won == false){
+            for (let i = 0; i < 3; i++){
+                winChecker = '';
+                for (let j = 0; j < 3; j++){
+                    winChecker += boardArray[i][j]
+                }
+                if (winChecker == 'oo-'){
+                    markCompMove(i,2);
+                    won = true;
+                    break
+                } else if (winChecker == 'o-o'){
+                    markCompMove(i,1);
+                    won = true;
+                    break
+                } else if (winChecker == '-oo'){
+                    markCompMove(i,0);
+                    won = true;
+                    break
+                }
+            }
+        }
+
+        //check columns for blocking move
+        if (won == false){
+            for (let i = 0; i < 3; i++){
+                winChecker = '';
+                for (let j = 0; j < 3; j++){
+                    winChecker += boardArray[j][i]
+                }
+                if (winChecker == 'oo-'){
+                    markCompMove(2,i);
+                    won = true;
+                    break
+                } else if (winChecker == 'o-o'){
+                    markCompMove(1,i);
+                    won = true;
+                    break
+                } else if (winChecker == '-oo'){
+                    markCompMove(0,i);
+                    won = true;
+                    break
+                }
+            }
+        }
+
+        //check diagonals for blocking move
+        if (won == false){
+            winChecker = '';
+
+            for (let i = 0; i < 3; i++){
+                winChecker += boardArray[i][i];
+            }
+                if (winChecker == 'oo-'){
+                    markCompMove(2,2);
+                    won = true;
+                } else if (winChecker == 'o-o'){
+                    markCompMove(1,1);
+                    won = true;
+                } else if (winChecker == '-oo'){
+                    markCompMove(0,0);
+                    won = true;
+                }
+        }
+
+        if (won == false){
+            winChecker = '';
+            
+            for (let i = 0; i < 3; i++){
+                winChecker += boardArray[2-i][i];
+            }
+                if (winChecker == 'oo-'){
+                    markCompMove(0,2);
+                    won = true;
+                } else if (winChecker == 'o-o'){
+                    markCompMove(1,1);
+                    won = true;
+                } else if (winChecker == '-oo'){
+                    markCompMove(2,0);
+                    won = true;
+                }
+        }
+
+        //if no winner placed, proceed to checking for block
+        if (won == false){
+            checkCompBlock();
         };
+    };
+
+    const checkCompBlock = function () {
+        let blockChecker = '';
+        let blocked = false;
+
+        //check rows for blocking move
+        if (blocked == false){
+            for (let i = 0; i < 3; i++){
+                blockChecker = '';
+                for (let j = 0; j < 3; j++){
+                    blockChecker += boardArray[i][j]
+                }
+                if (blockChecker == 'xx-'){
+                    markCompMove(i,2);
+                    blocked = true;
+                    break
+                } else if (blockChecker == 'x-x'){
+                    markCompMove(i,1);
+                    blocked = true;
+                    break
+                } else if (blockChecker == '-xx'){
+                    markCompMove(i,0);
+                    blocked = true;
+                    break
+                }
+            }
+        }
+
+        //check columns for blocking move
+        if (blocked == false){
+            for (let i = 0; i < 3; i++){
+                blockChecker = '';
+                for (let j = 0; j < 3; j++){
+                    blockChecker += boardArray[j][i]
+                }
+                if (blockChecker == 'xx-'){
+                    markCompMove(2,i);
+                    blocked = true;
+                    break
+                } else if (blockChecker == 'x-x'){
+                    markCompMove(1,i);
+                    blocked = true;
+                    break
+                } else if (blockChecker == '-xx'){
+                    markCompMove(0,i);
+                    blocked = true;
+                    break
+                }
+            }
+        }
+
+        //check diagonals for blocking move
+        if (blocked == false){
+            blockChecker = '';
+
+            for (let i = 0; i < 3; i++){
+                blockChecker += boardArray[i][i];
+            }
+                if (blockChecker == 'xx-'){
+                    markCompMove(2,2);
+                    blocked = true;
+                } else if (blockChecker == 'x-x'){
+                    markCompMove(1,1);
+                    blocked = true;
+                } else if (blockChecker == '-xx'){
+                    markCompMove(0,0);
+                    blocked = true;
+                }
+        }
+
+        if (blocked == false){
+            blockChecker = '';
+            
+            for (let i = 0; i < 3; i++){
+                blockChecker += boardArray[2-i][i];
+            }
+                if (blockChecker == 'xx-'){
+                    markCompMove(0,2);
+                    blocked = true;
+                } else if (blockChecker == 'x-x'){
+                    markCompMove(1,1);
+                    blocked = true;
+                } else if (blockChecker == '-xx'){
+                    markCompMove(2,0);
+                    blocked = true;
+                }
+        }
+
+        // on turn 2 if no block exists take edgecase moves
+        if (blocked == false && compTurn === 2){
+            turnTwoCompMoves();
+        }
+    };
+
+    const turnTwoCompMoves = function () {
+        let markerNotPlaced = true;
+        let emptyRow = 3;
+        let emptyColumn = 3;
+
+        // if human has taken opposite corners, take random edge
+        if ((boardArray[0][0] == 'x' && boardArray[2][2] == 'x') ||
+                (boardArray[0][2] == 'x' && boardArray[2][0] == 'x')){
+                let num1 = Math.floor(Math.random() * 4);
+                if (num1 == 0){
+                    markCompMove(0,1);
+                } 
+                else if (num1 == 1){
+                    markCompMove(1,0);
+                }
+                else if (num1 == 2){
+                    markCompMove(1,2);
+                }
+                else {
+                    markCompMove(2,1);
+                }
+        }
+        // else if human has taken corner and non-adjacent edge, take edge in empty row or column
+        else {
+            let emptyCheck = '';
+
+            //if empty row exists place in middle of row
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++){
+                    emptyCheck += boardArray[i][j];
+                }
+                if (emptyCheck == 'xox'){
+                    let num1 = Math.floor(Math.random() * 2) * 2;
+                    let num2 = Math.floor(Math.random() * 2) * 2;
+                    markCompMove(2 - num1,2 - num2);
+                    markerNotPlaced = false;
+                }
+                else if (emptyCheck == '---'){
+                    emptyRow = i
+                } 
+                emptyCheck = '';
+            }
+            if (markerNotPlaced && emptyRow != 3) {
+                markCompMove(emptyRow,1)
+            }
+
+            //if empty column exists place in middle of column
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++){
+                    emptyCheck += boardArray[j][i];
+                }
+                if (emptyCheck == 'xox'){
+                    let num1 = Math.floor(Math.random() * 2) * 2;
+                    let num2 = Math.floor(Math.random() * 2) * 2;
+                    markCompMove(2 - num1,2 - num2);
+                    markerNotPlaced = false;
+                }
+                else if (emptyCheck == '---'){
+                    emptyColumn = i
+                } 
+                emptyCheck = '';
+            }
+            if (markerNotPlaced && emptyColumn != 3) {
+                markCompMove(1,emptyColumn)
+            }
+
+        }
+
+    };
+
+    const markCompMove = function (x, y) {
+        //mark dom
+        let marker = document.createElement('img');
+        marker.setAttribute('src', 'Assets/o_icon.svg');
+        document.querySelector(`#row${x} :nth-child(${y + 1})`).appendChild(marker);
+
+        //mark move in boardArray
+        boardArray[x][y] = 'o';
+        checkWinner();
+        active_player = 'x';
+    }
+
+    const takeCompTurn = function () {
+
+        // turn 1
+        if (compTurn === 1) {
+
+            // if human non-center start, take center
+            // else take random corner
+            if (boardArray[1][1] != 'x'){
+                markCompMove(1,1);
+            } else {
+                let num1 = Math.floor(Math.random() * 2) * 2;
+                let num2 = Math.floor(Math.random() * 2) * 2;
+                markCompMove(2 - num1,2 - num2);
+            }
+        }
+
+        // turns after 1
+        if (compTurn > 1) {
+            checkCompWin();
+        }
     };
 
     const markBoard = function (e) {
@@ -195,8 +472,6 @@ let gameBoard = function () {
             colIndex = [...e.target.parentElement.children].indexOf(e.target);
             rowIndex = [...e.target.parentElement.parentElement.children].indexOf(e.target.parentElement);
             boardArray[rowIndex][colIndex] = 'x';
-            console.log(boardArray);
-            console.log(boardArray[1][1])
             checkWinner();
             active_player = 'o';
             compTurn++;
@@ -212,7 +487,11 @@ let gameBoard = function () {
             active_player = 'x';
         }
         e.target.appendChild(marker);
-        takeCompTurn();
+        
+        //take computer turn if relevant
+        if (gameType === 'pvc') {
+            takeCompTurn();
+        };
 
         //need to remove listener from marked square in dom
     };
@@ -242,14 +521,7 @@ let gameBoard = function () {
         };
 
         //remove markers from boardArray
-        // initBoardArray();
-        boardArray = new Array(3);
-        for (let i = 0; i < 3; i++) {
-            boardArray[i] = new Array(3);
-            for (let j = 0; j < 3; j++){
-                boardArray[i][j] = '-'
-            }
-        }
+        initBoardArray();
 
         //remove listeners from all game squares
         removeGameSquareListeners();
@@ -264,92 +536,12 @@ let gameBoard = function () {
     };
 }();
 
-let computerPlayer = function () {
-
-    let markCompMove = function (x, y) {
-        //mark dom
-        let marker = document.createElement('img');
-        marker.setAttribute('src', 'Assets/o_icon.svg');
-        document.querySelector(`#row${x} :nth-child(${y + 1})`).appendChild(marker);
-
-        console.log(gameBoard.boardArray)
-        gameBoard.boardArray[x][y] = 'o';
-        console.log(gameBoard.boardArray)
-        gameBoard.checkWinner();
-        active_player = 'x';
-    }
-
-    let checkBlock = function () {
-        let blockChecker = '';
-        //check horizontal winner
-        // for (let i = 0; i < 3; i++){
-        //     if (gameBoard.boardArray[i][1] === 'x') {
-        //         if (gameBoard.boardArray[i][0] === 'x' &&
-        //             gameBoard.boardArray[i][2] !== 'x'){
-        //                 markCompMove(i,2);
-        //                 break;
-        //         } 
-        //         else if (gameBoard.boardArray[i][2] === 'x' &&
-        //                 gameBoard.boardArray[i][0] !== 'x'){
-        //                     markCompMove(i,0);
-        //                     break;
-        //         }
-        //     }        
-        // }
-
-        for (let i = 0; i < 3; i++){
-            blockChecker = gameBoard.boardArray[i][0] + gameBoard.boardArray[i][1] + gameBoard.boardArray[i][2];
-            console.log(blockChecker);
-            console.log(gameBoard.boardArray);
-        } 
-
-        //check vertical winner
-        // for (let i = 0; i < 3; i++){
-        //     if (gameBoard.boardArray[1][i] === 'x') {
-        //         if (gameBoard.boardArray[0][i] === 'x' &&
-        //             gameBoard.boardArray[2][i] !== 'x'){
-        //                 markCompMove(2,i);
-        //                 break;
-        //         } 
-        //         else if (gameBoard.boardArray[2][i] === 'x' &&
-        //                 gameBoard.boardArray[0][i] !== 'x'){
-        //                     markCompMove(0,i);
-        //                     break;
-        //         }
-        //     }        
-        // }
-
-    }
-
-    let takeTurn = function () {
-
-        // turn 1
-        if (compTurn === 1) {
-
-            // player 1 non-center start
-            console.log(gameBoard.boardArray[1][1])
-            console.log(gameBoard.boardArray)
-            if (gameBoard.boardArray[1][1] != 'x'){
-                markCompMove(1,1);
-            } else {
-                markCompMove(0,0); 
-            }
-        }
-
-        // turn 2
-        if (compTurn === 2) {
-            checkBlock();
-        }
-    };
-
-    return {
-        takeTurn
-    }
-} ();
-
+createBoard();
+listeners.addGameSquareListeners();
+listeners.addNewBtnListener();
 
 // todo:
-// check block func
+// build out turn 3 and turn 4 edge case moves
 
 // build gameover logic
 // Call out winner and end game in check winner function
@@ -358,14 +550,3 @@ let computerPlayer = function () {
 // build computer logic
 // make it so markers can't be placed twice in the same square
 // disable gametype switching midgame
-
-
-
-
-
-
-
-createBoard();
-// gameBoard.initBoardArray();
-listeners.addGameSquareListeners();
-listeners.addNewBtnListener();
